35b86d4c0554acb122191731016fc87d628a12be
Anca1311 <100627032+Anca1311@users.noreply.github.com>
2022-02-28 22:57:11 +0200

    Update Application.cpp

--- Application.cpp
+++ Application.cpp
@@ -38,6 +38,50 @@ void Application::Menu() {
 				if (selectedOption == 3) flag = 13;
 				if (selectedOption == 4) flag = 14;
 			}
+		case 2:
+			cout << "Operationl: \n";
+			cout << "0. Back \n";
+			cout << "1. Select the family member \n";
+			selectOption();
+			if (selectedOption == 0) flag = -1;
+			else {
+				if (selectedOption == 1) {
+					flag = 21;
+					displayMembers();
+				}
+				selectOption();// Choosing the family member - ?
+				if (selectedOption == 0) flag = -1;
+				else {
+					OpenTableFor(selectedOption);
+				}
+				cout << "0. Back \n";
+				cout << "1. Reporting  \n";
+				cout << "2. Add income \n";
+				cout << "3. Add expense \n";
+				if (selectedOption == 0) flag = -1;
+				else {
+					if (selectedOption == 1){
+						flag = 211;
+						cout << "Reporting: \n";
+						cout << "0. Back \n";
+						cout << "1. View remaining budget \n";
+						cout << "2. View incomes \n";
+						cout << "3. View expenses \n";
+						cout << "4. Aggregated statistics \n";
+						selectOption();
+						if (selectedOption == 0) flag = -1;
+						else {
+							if (selectedOption == 1) flag = 2111;
+							if (selectedOption == 2) flag = 2112;
+							if (selectedOption == 3) flag = 2113;
+							if (selectedOption == 4) flag = 2114;
+						}
+					if (selectedOption == 2) flag = 212;
+					if (selectedOption == 3) flag = 213;
+					}
+			
+				}			
+			}
 		case 0:
 			break;
 		}
